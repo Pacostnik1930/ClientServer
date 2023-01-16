adc542631fadad4449e123de2fe4ef91ca5781a9
Петр < >
2023-01-16 19:54:56 +0300

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Client
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Это наш клиент");
+
+        }
+
+
+    }
+}
